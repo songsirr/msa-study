02bf30d486c7d12ab558d14fc132b6fa547a0eb8
songsirr <fkshwy@naver.com>
2022-04-08 19:14:59 +0900

    [TEST]create util lib and import from service module

--- msa/microservices/post-service/src/main/java/mj/song/microservices/core/post/controller/TestController.java
+++ msa/microservices/post-service/src/main/java/mj/song/microservices/core/post/controller/TestController.java
@@ -1,5 +1,7 @@
 package mj.song.microservices.core.post.controller;
 
+import mj.song.util.http.ServiceUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    private final ServiceUtil serviceUtil;
+
+    @Autowired
+    public TestController(ServiceUtil serviceUtil){
+        this.serviceUtil = serviceUtil;
+    }
+
     @GetMapping(
             value = "/post/{postId}",
             produces = "application/json")
     public String getPost(@PathVariable long postId){
-        return "test with " + postId + "\n";
+
+        return "test with " + postId + serviceUtil.getServiceAddress() + "\n";
     }
 }
