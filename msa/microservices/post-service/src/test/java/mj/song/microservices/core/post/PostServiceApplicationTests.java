17dc20617593a0659bf08021e5da3c8c3640ab16
songsirr <fkshwy@naver.com>
2022-04-08 23:08:24 +0900

    re commit

---
+++ msa/microservices/post-service/src/test/java/mj/song/microservices/core/post/PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mj.song.microservices.core.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
