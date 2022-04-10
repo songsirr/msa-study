bc016ca764fe2ec075d72975eff514a443e3e042
songsirr <fkshwy@naver.com>
2022-04-11 00:38:22 +0900

    update test bash (for docker)

--- msa/test-em-all.bash
+++ msa/test-em-all.bash
@@ -5,7 +5,7 @@
 #   HOST=localhost PORT=7000 ./test-em-all.bash
 #
 : ${HOST=localhost}
-: ${PORT=7002}
+: ${PORT=8080}
 
 function assertCurl() {
 
@@ -44,11 +44,54 @@ function assertEqual() {
     exit 1
   fi
 }
+
+function testUrl() {
+    url=$@
+    if curl $url -ks -f -o /dev/null
+    then
+          echo "Ok"
+          return 0
+    else
+          echo -n "not yet"
+          return 1
+    fi;
+}
+
+function waitForService() {
+    url=$@
+    echo -n "Wait for: $url... "
+    n=0
+    until testUrl $url
+    do
+        n=$((n + 1))
+        if [[ $n == 100 ]]
+        then
+            echo " Give up"
+            exit 1
+        else
+            sleep 6
+            echo -n ", retry #$n "
+        fi
+    done
+}
+
 set -e
+
+echo "Start:" `date`
 
 echo "HOST=${HOST}"
 echo "PORT=${PORT}"
 
+if [[ $@ == *"start"* ]]
+then
+    echo "Restarting the test environment..."
+    echo "$ docker-compose down"
+    docker-compose down
+    echo "$ docker-compose up -d"
+    docker-compose up -d
+fi
+
+waitForService http://$HOST:$PORT/management/1
 
 # Verify that a normal request works, expect three posts
 assertCurl 200 "curl http://$HOST:$PORT/management/1 -s"
@@ -61,3 +104,12 @@ assertCurl 404 "curl http://$HOST:$PORT/management/404 -s"
 # Verify that a 422 (Unprocessable Entity) error is returned for a userId 0
 assertCurl 422 "curl http://$HOST:$PORT/management/0 -s"
 assertEqual "\"Invalid id:0\"" "$(echo $RESPONSE | jq .message)"
+
+if [[ $@ == *"stop"* ]]
+then
+    echo "We are done, stopping the test environment..."
+    echo "$ docker-compose down"
+    docker-compose down
+fi
+
+echo "End:" `date`
